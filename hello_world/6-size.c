49f3b35e3fa944bff3870bd52c7023098edf7610
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-18 13:12:58 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of char:%ld bytes(s)\n", sizeof(char));
-	printf("Size of int:%ld bytes(s)\n", sizeof(int));
-	printf("Size of long int: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of long long int: %ld bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %ld bytes(s)\n", sizeof(float));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)\n", sizeof(float));
 	return (0);
  }
