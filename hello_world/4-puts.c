e9c854577e8c3b18737e73597cd237982bc81d14
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-18 05:07:03 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #!/bin/bash
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  * /
  
-int main()
-{char* str1 = "\"Programming is like building a multilingual puzzle";
-	puts(str1);
-	return 0;
+int main(void)
+{puts{"\"Programming is like building a multilingual puzzle");
+	return (0);
 }
