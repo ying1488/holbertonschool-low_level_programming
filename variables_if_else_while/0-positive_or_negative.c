2288804081861db825c898b5300d1a6e1fdf7984
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-18 21:38:46 +0000

    readme

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * description: main
+ * Return: Always 0 (Success)
+ */
+
+int main(void)i
+{
+	int n;
+	/* if the number is greater than 0: is positive */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+
+	/* If the number is less than 0: is negative */
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	/* if the number is 0: is zero */
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
