7f7514438b6a79a7d27c8de6fbc4a94ebe3f7766
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-19 05:04:04 +0000

    mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints a text according number
+ * description : main
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha => 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+i}
