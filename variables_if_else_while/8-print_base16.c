a1e32e8bf79d8950a49c637f099b361a3b661375
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-19 05:22:27 +0000

    base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  *main - prints numbers of base 16 in lowercase
+  *Return: 0
+*/
+
+int main(void)
+{
+    char num;
+
+    for (num = '0'; num <= '9'; num++)
+    {
+        putchar(num);
+    }
+
+    for (num = 'a'; num <= 'f'; num++)
+    {
+	putchar(num);
+    }
+    putchar('\n');
+    return (0);
+}
