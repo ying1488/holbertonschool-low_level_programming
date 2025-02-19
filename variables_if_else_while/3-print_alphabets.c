2853fa0d0b7d25f0df9242deb798dbaadedd9d77
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-19 03:49:56 +0000

    lower case and upper case

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints a text according number
+ * description : main
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++) 
+{
+		putchar(alpha);
+	}
+	for (alpha = 'A'; alpha <='Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n')
+
+	return (0);
+}
