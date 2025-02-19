42c0066b01dc60965987a15cb7ac1c5534083ad1
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-19 04:30:01 +0000

    single base

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print all possible combinations of single-digit numbers.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
