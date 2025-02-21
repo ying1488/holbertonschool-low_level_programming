09d980bdfbb9f4dc7c3cd6c9fd6b9c816e9b8770
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-21 08:41:11 +0000

    print alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet, in lowercase
+ * Description : prints the alphabet, in lowercase
+ * Return : Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha ++)
+	{
+		putchar(alphabet);
+	}
+		
+	putchar('\n')
+	return (0);
+}
