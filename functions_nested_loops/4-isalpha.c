e92489d3b9174f8badf9c2586f735d1e7bd38de2
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-21 12:14:46 +0000

    is lower upper and a letter

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,14 +2,14 @@
 #include "main.h"
 
 /**
- * * _isalpha - checks for letter,lowercase or uppercase
+ * _isalpha - checks for letter,lowercase or uppercase
  * @c: the character to check
  * Return: Always (Success)
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 122)
+	if ((c >= 65 && c <= 91) || (c >= 97 && c <= 122))
 	{
 	return (1);
 	}
