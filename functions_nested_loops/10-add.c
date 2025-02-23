84dc9459eb5b6fad4eaca27c09101829bdb95593
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-23 04:46:33 +0000

    add 2 numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add  -  a function that adds two integers and returns the result.
+ * @x: int to be added to x
+ * @y: int to be added to y
+ * Return -  sum of x and y
+*/
+
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+
+	return (z);
+}
