0cc4e9f4efc97724d2a54cc982290f3feecdec1e
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-23 04:10:30 +0000

    time

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -20,6 +20,7 @@ void jack_bauer(void)
 			/* show time in hh:mm format */
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
+			_putchar(':');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');		
