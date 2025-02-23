7d5d4efcd124682b8a66f401989c855b68e26eb4
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-23 04:05:30 +0000

    time

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the date
+ * time format hh:mm
+ * Return: value of the last digit
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+	/* loop for hour */
+	i = 0;
+	while (i < 24)
+	{
+		/* loop for minute */
+		j = 0;
+		while (j < 60)
+		{
+			/* show time in hh:mm format */
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		i++;
+	}
+}
