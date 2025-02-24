21a09a6b2f8d1af4beee28244f55452b31b84042
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-24 07:02:19 +0000

    initial commit

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ * Return: Always (Success)
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+
+	return (1);
+	}
+
+	return (0);
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+C - More functions, more nested loops
+
+
+int _isupper(int c);
