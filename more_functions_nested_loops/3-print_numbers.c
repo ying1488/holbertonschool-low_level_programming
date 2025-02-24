40da1b9a3458f95d7741ef0dcdcf9c50c6c53c4e
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-24 11:34:41 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line.
+ * Return: numbers 0-9
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		_putchar(x + '0');
+	}
+
+	_putchar('\n');
+}
