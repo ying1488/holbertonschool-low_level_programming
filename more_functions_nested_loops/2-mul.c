24dd80497716300de2bf2bd80e4b7c59b0d9e454
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-24 07:19:18 +0000

    multiplication

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul  -  a function that multiplies two integers and returns the result.
  * @a: int to be added to a
  * @b: int to be added to b
- * Return -  mul of x and y
+ * Return -  multiplication of a and b
 */
 
 int mul(int a, int b)
