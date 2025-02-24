8d73d0b71e3ed9c1b708c5a65ee6e6598435abf3
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-24 07:08:24 +0000

    is a number

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit.
+ * @c: the character to check
+ * Return: Always (Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+
+	return (1);
+	}
+
+	return (0);
+}
