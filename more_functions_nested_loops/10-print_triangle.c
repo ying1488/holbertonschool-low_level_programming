e64770ebc21235cfa94c6e0942e49bfdb4d741a0
root <yingintech@gmail.com  git config --global user.name Ying>
2025-02-25 10:57:50 +0000

    print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
+  */
+
+
+void print_triangle(int size)
+{
+	int i, j, space; 
+	
+	while (size != 0) 
+	{
+	for (i = 0; i <= size; i++)
+		{
+		for (space = size; space <= (size - i); space--)
+			{
+				_putchar(32);
+			}
+		for (j = 0; j <= size; j++)
+			{
+				_putchar(42);
+			}
+		_putchar("\n")
+		}	
+	}
+}
