1bb88371fca764d95e29999dd0e9d1fd3f88186c
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-02 05:19:52 +0000

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * void more_numbers(void) - prints 10 times the numbers, from 0 to 14
+ * Return: Always (Success)
+ */
+
+void more_numbers(void)
+{
+	char n;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (n = 0 ; n <= 14 ; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+		i++;
+	}
+	
+}
