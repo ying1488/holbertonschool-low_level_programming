bb5b6810fbaebd3b040474829048fe53e6afa2cf
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-02 08:06:42 +0000

    reset to 98

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98.
+ * reset_to_98 - function that takes a pointer and updates to 98.
  * @n: parameter targeted.
  * Return: Always 0.
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;	
+	*n = 98;
 }
