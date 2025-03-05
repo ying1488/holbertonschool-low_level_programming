c53dfa60fbaf467856d645b3904ff62a139435b4
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-05 13:27:36 +0000

    fixzed betty

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -12,26 +12,26 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i=0; 
-    
-    while (haystack[i])
-    {
-        int j=0;
-        while(needle[j])
-        {
-            if(haystack[i+j] != needle [j])
-            {
-                break;
-            }
-            
-            j++;
-        }
-        if (needle[j] == '\0')
-        {
-            return (haystack + i);
-        }
-        
-        i++;
-    }
-    return('\0');
+	int i = 0;
+
+	while (haystack[i])
+	{
+		int j = 0;
+
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
+			j++;
+		}
+
+	if (needle[j] == '\0')
+	{
+		return (haystack + i);
+	}
+	i++;
+	}
+	return ('\0');
 }
