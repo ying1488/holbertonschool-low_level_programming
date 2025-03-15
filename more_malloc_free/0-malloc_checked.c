64e7c08a811ea7e675ada2f76c438c1fdd59e4f2
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-15 12:25:02 +0000

    betty fixed

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
  /**
-  * malloc_checked 2- allocates memory using malloc
+  * malloc_checked - allocates memory using malloc
   * @b: unsigned integer
-  * returns- pointer to the allocated memory
+  * returnsi - pointer to the allocated memory
   */
 
 void *malloc_checked(unsigned int b)
