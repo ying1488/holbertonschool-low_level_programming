466789fbb5c353f3a35291bf0c8827be1beaf91a
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-16 12:56:11 +0000

    freed dog memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free space
+ * @d: pointer to dog typedef type
+ * Return: Always success
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
