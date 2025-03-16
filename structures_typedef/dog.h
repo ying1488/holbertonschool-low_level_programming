e47cc23ac20656ba528c408285ff5b6dc79862a5
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-16 03:23:28 +0000

    struct dog established

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dogs
+ * @name: pointer to string
+ * @age : age is non int
+ * @owner: pointer to string
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
