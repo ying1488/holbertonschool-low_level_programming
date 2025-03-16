e3135f08d1ee0394d3542075877beefb57baf063
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-16 01:32:47 +0000

    concat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat- concatenates two strings.
+ * @n: int
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer shall point to a newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
+		len1++;
+
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+	result = malloc(sizeof(char) * (len1 + n + 1));
+
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
+
+	result[i + j] = '\0';
+
+	return (result);
+}
