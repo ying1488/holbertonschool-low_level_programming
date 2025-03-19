0430da8858460d880121648314c39e4778730ffb
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-19 12:06:59 +0000

    f name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name.
+ * @name: name
+ * @f: function
+ * Return: Always Success
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,73 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
+
+
+#endif /* MAIN_H */
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+~                                                                                                                                                                              
+"main.h" 13L, 226B                                                                                                                                           13,19         All
