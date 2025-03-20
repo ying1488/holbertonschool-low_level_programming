a74b4f9af77b5580bb9af01c1089406320f446df
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-20 03:33:33 +0000

    get_op_function established with all operations

---
+++ function_pointers/3-calc.h
@@ -0,0 +1,14 @@
+
+
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+ *op_add - functions that sums a and b.
+ *@a: int a
+ *@b: int b
+ *Return: result of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+
+/**
+ *op_sub - functions that subtract a and b.
+ *@a: int a
+ *@b: int b
+ *Return: result of operation
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ *op_mul- functions that multiplies a and b.
+ *@a: int a
+ *@b: int b
+ *Return: result of operation
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ *op_div - functions that divides a and b.
+ *@a: int a
+ *@b: int b
+ *Return: result of operation
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+
+/**
+ *op_mod - functions that sums a and b.
+ *@a: int a
+ *@b: int b
+ *Return: result of operation
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+
