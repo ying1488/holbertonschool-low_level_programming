cb697411c5d9e661af0c0bb28c4879414e5400cc
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-22 03:14:09 +0000

    array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * array_iterator - executes a function given as parameter
+ * @action: pointer to the function you need to use
+ * @size: size of array
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
