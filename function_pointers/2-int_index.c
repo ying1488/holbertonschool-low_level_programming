8ce7579d096deb5bbe6eb16e367f3e451fd8434c
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-22 06:40:35 +0000

    betty fixed

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
-
+/**
+ *int_index- function that searches for an integer.
+ *@array: array
+ *@size: size of array
+ *@cmp: compare function
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
@@ -11,14 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
-    
+
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
-			{
-				return (i);        
-			}
-		i++;	
+		{
+			return (i);
+		}
+		i++;
 	}
-	return -1;
+	return (-1);
 }
