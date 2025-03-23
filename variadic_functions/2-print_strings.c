2a0e1526dc89b4e875ce6ae8109d3b32c7aa7ef9
root <yingintech@gmail.com  git config --global user.name Ying>
2025-03-23 08:28:34 +0000

    betty fixed

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -17,13 +17,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 
 	va_start(args, n);
-	
+
 	for (i = 0; i < n ; i++)
 	{
 		s = va_arg(args, char *);
 
-		printf("%s",s);
-		
+		printf("%s", s);
+
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
