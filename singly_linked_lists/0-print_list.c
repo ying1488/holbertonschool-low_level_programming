c6f25428521ca47f6c85511b9c1520aeb5b72650
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-02 07:18:12 +0000

    betty fixed

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 
 
 /**
- * size_t print_list - function that prints all the elements of a list_t list.
+ * print_list - function that prints all the elements of a list_t list.
  * @h: pointer to head
  * Return: number of nodes
  */
