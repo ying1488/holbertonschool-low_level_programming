7bbbea3ba0f78c8be956b1dd35f63a4047eb66e9
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-02 07:15:37 +0000

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * size_t print_list - function that prints all the elements of a list_t list.
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+
+	{
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+
+}
