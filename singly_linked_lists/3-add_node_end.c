16b7a36bab73cbbaa7f1bc672e671e6a54500271
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-03 06:00:29 +0000

    add node to end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *add_node - adds a new node at the begining of a list
+ *@head: pointer to the list_k
+ *@str: string
+ *Return: the address of the new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *new;
+	list_t *temp;
+        int i = 0; /*pointer*/
+
+        new = malloc(sizeof(list_t));
+        if (new == NULL)
+        {
+                printf("Error\n");
+                return (NULL);
+        }
+
+        while (str[i] != '\0')
+        {
+                i = i + 1;
+        }
+
+        new->str = strdup(str);
+        new->len = i;
+        new->next = NULL;
+        
+	if(*head == NULL)
+	{
+	*head = new; /*return pointer to new element*/
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next !=NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	
+	}
+
+        return (new);
+}          
