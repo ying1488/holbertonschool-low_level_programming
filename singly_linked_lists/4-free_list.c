438495a1df0f8be5df15b571c9efe1f57c43ac20
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-03 06:48:48 +0000

    free memory

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ *free_list - frees a list  
+ *@head: head
+ *Return: new list
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);	
+	
+}
