4176f8b2bd607a1aa5727242c41832016f753c2b
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-06 11:40:42 +0000

    betty fixed

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -5,11 +5,10 @@
 
 
 /**
- *dlistint_len - returns the number of elements 
- in a linked dlistint_t list.
+ *dlistint_len - returns the number of elements in a linked list.
  *@h: const dlistint_t
  *Return: the number of elements in a linked dlistint_t
- */ 
+ */
 
 size_t dlistint_len(const dlistint_t *h)
 {
