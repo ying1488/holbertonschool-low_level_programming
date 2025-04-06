927cd42e9f5f3ff598b83e327e69e9dc0f1fe43b
root <yingintech@gmail.com  git config --global user.name Ying>
2025-04-06 13:29:18 +0000

    sum of elements

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - reutrns sum of all data
+ *@head: dlistint_t
+ * Return: sum of all data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while(head)
+	{
+	sum += head->n;
+	head = head->next;
+	}
+
+	return(sum);
+
+}
